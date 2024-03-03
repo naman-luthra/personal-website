a8b8a3b40415b40a62e28159c883f7d61d6d91c3
Naman Luthra <f20201682@pilani.bits-pilani.ac.in>
2024-03-03 10:48:07 +0530

    make profile pic hidden for mobile

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -34,7 +34,7 @@ const AboutSection = () => {
                     <div className="inline-block mt-8 p-3 rounded-md font-semibold text-white border-white border-2 hover:bg-white hover:text-matte-black">{getTranslatedContent("RESUME_BUTTON", "ABOUT", languageCode)}</div>
                 </a>
             </div>
-            <div className="basis-1/4 md:basis-1/3 lg:basis-1/2 relative">
+            <div className="basis-1/4 md:basis-1/3 lg:basis-1/2 relative hidden md:block">
                 <div className="hidden md:inline-block w-56 h-56 rounded-full bg-white opacity-70 absolute left-4 top-16"></div>
                 <img src="./icons/profilePic2023.png" className="w-36 sm:w-56 absolute top-12 right-4 md:left-0" alt="" />
                 <div className="w-screen h-[200vh] absolute bg-white opacity-80 rounded-lg -z-40 origin-top-left top-40 left-28 -rotate-45"/>
